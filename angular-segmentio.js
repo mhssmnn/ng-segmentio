403c1cbb96978b0d7273010e9b9691086119a059
Troex Nevelin <troex@fury.scancode.ru>
2014-05-03 00:58:44 +0300

    bump segmentio code to latest and greatest, keep segment.io versioning

--- angular-segmentio.js
+++ angular-segmentio.js
@@ -8,27 +8,49 @@ angular.module('segmentio', ['ng'])
             // Define a factory that generates wrapper methods to push arrays of
             // arguments onto our `analytics` queue, where the first element of the arrays
             // is always the name of the analytics.js method itself (eg. `track`).
-            var methodFactory = function(type) {
+            var methodFactory = function(method) {
                 return function() {
-                    var args = Array.prototype.slice.call(arguments, 0);
-                    $log.info('Call segmentio API with', type, args);
-                    if ($window.analytics.initialized) {
-                        $log.info('Segmentio API initialized, calling API');
-                        $window.analytics[type].apply($window.analytics, args);
-                    } else {
-                        $log.info('Segmentio API not yet initialized, queueing call');
-                        $window.analytics.push([type].concat(args));
-                    }
+                    var args = Array.prototype.slice.call(arguments);
+                    args.unshift(method);
+                    $window.analytics.push(args);
+                    return $window.analytics;
                 };
             };
 
             // Loop through analytics.js' methods and generate a wrapper method for each.
-            var methods = ['identify', 'track', 'trackLink', 'trackForm', 'trackClick',
-                'trackSubmit', 'page', 'pageview', 'ab', 'alias', 'ready', 'group'
+            var methods = ['identify', 'group', 'track', 'page', 'pageview', 'alias',
+                'ready', 'on', 'once', 'off', 'trackLink', 'trackForm', 'trackClick',
+                'trackSubmit'
             ];
             for (var i = 0; i < methods.length; i++) {
                 service[methods[i]] = methodFactory(methods[i]);
             }
+
+            /**
+             * @description
+             * Load Segment.io analytics script
+             * @param apiKey The key API to use
+             */
+            service.load = function(key) {
+                if (document.getElementById('analytics-js')) return;
+
+                // Create an async script element based on your key.
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.id = 'analytics-js';
+                script.async = true;
+                script.src = ('https:' === document.location.protocol
+                  ? 'https://' : 'http://')
+                  + 'cdn.segment.io/analytics.js/v1/'
+                  + key + '/analytics.min.js';
+
+                // Insert our script next to the first script element.
+                var first = document.getElementsByTagName('script')[0];
+                first.parentNode.insertBefore(script, first);
+            };
+
+            // Add a version to keep track of what's in the wild.
+            $window.analytics.SNIPPET_VERSION = '2.0.9';
 
             // Listening to $viewContentLoaded event to track pageview
             $rootScope.$on('$viewContentLoaded', function() {
@@ -37,24 +59,6 @@ angular.module('segmentio', ['ng'])
                     service.pageview(service.location);
                 }
             });
-
-            /**
-             * @description
-             * Load Segment.io analytics script
-             * @param apiKey The key API to use
-             */
-            service.load = function(apiKey) {
-                // Create an async script element for analytics.js.
-                var script = document.createElement('script');
-                script.type = 'text/javascript';
-                script.async = true;
-                script.src = ('https:' === document.location.protocol ? 'https://' : 'http://') +
-                    'd2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/' + apiKey + '/analytics.js';
-
-                // Find the first script element on the page and insert our script next to it.
-                var firstScript = document.getElementsByTagName('script')[0];
-                firstScript.parentNode.insertBefore(script, firstScript);
-            };
 
             return service;
         }
